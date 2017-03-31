37d8c507522248cb2683e68a88811f67a9dfc7e4
apple <apple@appledemacbook-pro.local>
2017-03-31 17:06:34 +0800

    优化！

--- CDDStoreDemo/CDDStoreDemo/Class/Others(其他)/Controller/AppDelegate.m
+++ CDDStoreDemo/CDDStoreDemo/Class/Others(其他)/Controller/AppDelegate.m
@@ -6,8 +6,6 @@
 //  Copyright © 2017年 apple. All rights reserved.
 //
 
-#define IOS_VERSION [[[UIDevice currentDevice] systemVersion] floatValue]
-
 #import "AppDelegate.h"
 #import "DCTabBarViewController.h"
 
@@ -20,10 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
-    if (IOS_VERSION > 9.0) {
-        [self init3DTouchActionShow:YES]; //关闭
-    }else{
-        [self init3DTouchActionShow:NO]; //关闭
+    if ([[[UIDevice currentDevice] systemVersion] intValue] > 9.0) {
+        [self init3DTouchActionShow:YES]; //打开
     }
     
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
